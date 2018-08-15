2940a3371ab32ab5a18b26c87c928a97cfc8a772
Matthew Crooks <mfcrooks@gmail.com>
2018-08-15 01:32:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function driversWithRevenueOver(driver, revenue) {
   });
 }
 
-/.
+//driverNamesWithRevenueOver() - This function works the same way as driversWithRevenueOver(). The difference is that it returns an array of strings representing the name of each driver who has a revenue greater than the specified amount. For example, driverNamesWithRevenueOver(drivers, 2000) will return ['Sammy'], as Sammy is the name of the matching driver object with a revenue greater than 2000.
 function driverNamesWithRevenueOver (drivers, revenue) {
   return driversWithRevenueOver(drivers, revenue)
     .map(function (driver) {
