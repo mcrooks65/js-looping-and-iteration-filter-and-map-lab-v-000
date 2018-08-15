86f695bcfa162f40e107541791b026d811c15a71
Matthew Crooks <mfcrooks@gmail.com>
2018-08-15 01:32:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // This function takes an array of driver JavaScript objects as the first argument and a string called revenue as the second argument. It returns an array of driver objects that have a revenue attribute that's greater than the passed-in revenue argument.
-function driversWithRevenueOver(driver, revenue) { 
+function driversWithRevenueOver(driver, revenue) {
   return driver.filter(function (driver) {
     return driver.revenue > revenue;
   });
-} 
+}
 
+/.
 function driverNamesWithRevenueOver (drivers, revenue) {
   return driversWithRevenueOver(drivers, revenue)
     .map(function (driver) {
